1b8dabdb6b34a201f1a1209fc7eb09b38e133bb3
HugoMichard <hugomichard@yahoo.com>
2018-07-05 13:22:52 +0200

    preparing transitions

---
+++ js/transition/transition.js
@@ -0,0 +1,47 @@
+/*
+$(".hero-slides a").on("click", function(e) {
+  e.preventDefault();
+  $(".portfolio-area").fadeOut();
+  $(".sonar-call-to-action-area").fadeOut();
+  $(".footer-area").fadeOut();
+  if ( $( "div:first" ).is( ":hidden" ) ) {
+    $( "div" ).show( "slow" );
+  } else {
+    $( "div" ).slideUp(3000);
+  }
+  setTimeout(relocate, 3000);
+});
+
+function relocate(){
+    document.location.href = "montage.html";
+}
+*/
+/*
+$(function(){
+  'use strict';
+    var options = {
+        debug: true,
+        prefetch: true,
+        cacheLength: 2,
+        onStart: {
+          duration: 2000, // Duration of our animation
+          render: function ($container) {
+            // Add your CSS animation reversing class
+            $("#main").addClass('fadeOut');
+            // Restart your animation
+            smoothState.restartCSSAnimations();
+          }
+        },
+        onReady: {
+          duration: 0,
+          render: function ($container, $newContent) {
+            // Remove your CSS animation reversing class
+            $("#main").removeClass('fadeOut');
+            // Inject the new content
+            //$container.html($newContent);
+          }
+        }
+      },
+      smoothState = $("#launcher").smoothState(options).data('smoothState');
+});
+*/
